94cd2412e2ed2ec74221eef393cbce1b680c2295
dongzhimin <dongzhimin@proton.me>
2025-08-13 01:21:15 +0800

    九宫格修复

--- app/models/status.rb
+++ app/models/status.rb
@@ -44,7 +44,7 @@ class Status < ApplicationRecord
   include Status::ThreadingConcern
   include Status::Visibility
 
-  MEDIA_ATTACHMENTS_LIMIT = 4
+  MEDIA_ATTACHMENTS_LIMIT = 12
 
   QUOTE_APPROVAL_POLICY_FLAGS = {
     unknown: (1 << 0),
