338757c996a505b4ba94acdd71f3d82c4020d6b5
dongzhimin-xz <dongzhimin@proton.me>
2025-08-21 02:23:20 +0800

    Update routing_helper.rb
    Fix the bucket url leaking

--- app/helpers/routing_helper.rb
+++ app/helpers/routing_helper.rb
@@ -23,7 +23,7 @@ module RoutingHelper
   def expiring_asset_url(attachment, expires_in)
     case Paperclip::Attachment.default_options[:storage]
     when :s3, :azure
-      attachment.expiring_url(expires_in.to_i)
+      URI.parse(attachment).host.replace(ENV.fetch('S3_ALIAS_HOST'))
     when :fog
       if Paperclip::Attachment.default_options.dig(:fog_credentials, :openstack_temp_url_key).present?
         attachment.expiring_url(expires_in.from_now)
